9e64382d116f36754dd9c2b2a7fa3a437cab3937
Mohammed <74480856+MohammedSaleh2001@users.noreply.github.com>
2022-11-21 16:20:37 -0700

    Added checks for NULLs in article search

--- searches.py
+++ searches.py
@@ -14,10 +14,13 @@ def searchArticle(collection, keywords):
     count = 0
     for result in results:
         print("\nArticle #" + str(count+1))
-        print("\tID: " + result["id"] + 
-            "\n\tTitle: " + result["title"] + 
-            "\n\tYear: " + result["year"] + 
-            "\n\tVenue: " + result["venue"] + "\n")
+        if (result["id"] is not None): print("\tID: " + result["id"])
+        if (result["title"] is not None): print("\tTitle: " + result["title"])
+        if (result["year"] is not None):print("\tYear: " + result["year"])
+        if (result["venue"] is not None): 
+            print("\tVenue: " + result["venue"] + "\n")
+        else:
+            print("\tVenue: NA")
         count += 1
     while (1):
         x = input("Would you like to see the full details of an article ? ! ? (Y/N): ")
@@ -31,12 +34,15 @@ def searchArticle(collection, keywords):
                 continue
             else:
                 print("\nArticle #" + y + ":")
-                print("\tID: " + results[int(y)-1]["id"] + 
-                    "\n\tTitle: " + results[int(y)-1]["title"] + 
-                    "\n\tYear: " + results[int(y)-1]["year"] + 
-                    "\n\tVenue: " + results[int(y)-1]["venue"])
+                if (results[int(y)-1]["id"] is not None): print("\tID: " + results[int(y)-1]["id"]) 
+                if (results[int(y)-1]["title"] is not None): print("\tTitle: " + results[int(y)-1]["title"]) 
+                if (results[int(y)-1]["year"] is not None): print("\tYear: " + results[int(y)-1]["year"]) 
+                if (results[int(y)-1]["venue"] is not None): print("\tVenue: " + results[int(y)-1]["venue"])
                 if ("abstract" in results[int(y)-1]): 
-                    print("\tAbstract: " + results[int(y)-1]["abstract"])
+                    if (results[int(y)-1]["abstract"] is not None): 
+                        print("\tAbstract: " + results[int(y)-1]["abstract"])
+                    else:
+                        print("\tAbstract: NA")
                 authnum = 1
                 for author in results[int(y)-1]["authors"]:
                     print("\tAuthor #" + str(authnum) + ": " + author)
@@ -46,9 +52,9 @@ def searchArticle(collection, keywords):
                 for reference in subcursor:
                     print("References to Article " + y + ":")
                     print("Reference #: " + str(refnum))
-                    print("\tID: " + reference["id"] + 
-                        "\n\tTitle: " + reference["title"] + 
-                        "\n\tYear: " + reference["year"])
+                    if (reference["id"] is not None): print("\tID: " + reference["id"])
+                    if (reference["title"] is not None): print("\tTitle: " + reference["title"])
+                    if (reference["year"] is not None): print("\tYear: " + reference["year"])
                     refnum += 1
 
 
